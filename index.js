66e942b4344f5d0ae0c0187c82ff17c31009a7d9
Javier Alba <jalba@Javiers-MacBook-Pro.local>
2014-11-18 23:45:44 +0000

    ready

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-var args = process.argv.slice(2),
-    concat = require('concat'),
-    files = args.slice(0,-1),
-    output = args[1];
-//module.exports = function() {
-    /*concat(files, output, function (error) {
-      if(error) console.error(error);
-      console.log('done');
-    });*/
-//};
-console.log(files);
+#!/usr/bin/env node
+var concat = require('concat'),
+    args = process.argv,
+    files,
+    output,
+    dest,
+    filesIdx = args.indexOf('-f') > args.indexOf('--files') ? 
+               args.indexOf('-f') : args.indexOf('--files'),
+    outputIdx = args.indexOf('-o') > args.indexOf('--output') ? 
+                args.indexOf('-o') : args.indexOf('--output');
+ 
+filesIdx < 0 && console.log('files parameter missing') && process.exit(1);
+
+outputIdx < 0 ? (outputIdx = args.length) && (dest = 'all.') : dest = args[args.length - 1];
+
+files = args.slice(filesIdx+1, outputIdx);
+
+dest === 'all.' && (dest += files[0].split('.')[1]);
+ 
+concat(files, dest, function (error) {
+  if(error) {
+   	console.error(error);
+   	process.exit(2);
+   	return;
+  }
+  console.log('files concatenated');
+  process.exit(0);
+});
+
+
+
